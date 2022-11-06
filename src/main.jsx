f700b8dca063cfd9bd30c96c2bec055a8bd2db4b
beamingdeamon <51133964+beamingdeamon@users.noreply.github.com>
2022-11-07 01:18:50 +0600

    dsad

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App"
 import Home from "./routes/Home"
 import "./tailwind.css"
 import GetUserData from "./routes/GetUserData"
+import GetSignature from "./routes/GetSignature"
 import TransferView from "./routes/TransferView"
 import C2C from "./routes/C2C"
 import QrCodePage from "./routes/QrCodePage"
@@ -19,7 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             <Route path="/" element={<App />}>
                 <Route path="" element={<Home />} />
                 <Route path="from-kotlin/:phone_number" element={<GetUserData />}></Route>
-                <Route path="get-signature/:signature" element={<GetUserData />}></Route>
+                <Route path="get-signature/:signature" element={<GetSignature />}></Route>
                 <Route path="transfer" element={<TransferView />}></Route>
                 <Route path="c2c" element={<C2C />}></Route>
                 <Route path="succes-transfer" element={<SuccessfullTransfer />}></Route>
