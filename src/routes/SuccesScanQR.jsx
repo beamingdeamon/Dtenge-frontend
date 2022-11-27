1d64d01d39d7f95495ba8a8bcc5225891401b241
beamingdeamon <51133964+beamingdeamon@users.noreply.github.com>
2022-11-28 02:45:26 +0600

    dsada

--- src/routes/SuccesScanQR.jsx
+++ src/routes/SuccesScanQR.jsx
@@ -6,9 +6,10 @@ import axios from "axios"
 export default function SuccesScanQR() {
   const navigateTo = useNavigate();
   const API_URL = "https://dtenge.eubank.kz:8000/api/"
-  const [transferResponse, setTransferResponse] = useState({status : 200})
+  const [transferResponse, setTransferResponse] = useState({status : 201})
+  const [isRequest, setIsRequest] = useState(false)
   useEffect(() => {
-     transfer()
+    //  transfer()
 }, [])
     const transfer = () =>{
         axios.post(API_URL + "qr-transfer/", { 
@@ -24,6 +25,7 @@ export default function SuccesScanQR() {
           }).then((res) => {
               const response = res.data
               setTransferResponse(response)
+              setIsRequest(true)
               console.log(response)
           })
     }
@@ -35,7 +37,64 @@ export default function SuccesScanQR() {
     <div>
         <div className="h-screen bg-gray-800">
             {
-                transferResponse.status != 200 ? (
+                !isRequest ? (
+                    <div className="p-4">
+                        <div className="mb-5 flex justify-end">
+                        <a href="#">
+                            <i className="far fa-times text-2xl text-gray-400" onClick={goBack}></i>
+                        </a>
+                        </div>
+                        <div className="mb-8 text-center">
+                            <i className="fas fa-spinner mb-6 text-[60px] text-yellow-500"></i>
+                            <div className="mb-1 text-center text-2xl font-bold text-white">Ожидание</div>
+                        </div>
+                        <div className="mb-10 rounded-xl bg-gray-700 p-4">
+                        <div className="flex flex-col gap-5">
+                            <div className="flex items-center">
+                            <div className="flex grow items-center">
+                                <div className="mr-3 flex h-[32px] w-[32px] items-center justify-center rounded-full bg-[#51819C] text-white">{window.localStorage.getItem("reciever-name").charAt(0)}</div>
+                                <div>
+                                <div className="text-sm font-semibold text-white">{window.localStorage.getItem("reciever-name")}</div>
+                                <div className="text-xs text-gray-400">Сохранить в избранное</div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col items-end">
+                                <label for="default-toggle" className="relative inline-flex cursor-pointer items-center">
+                                <input type="checkbox" value="" id="default-toggle" className="peer sr-only" />
+                                <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-green-600 peer-checked:after:translate-x-full peer-checked:after:border-white"></div>
+                                </label>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="rounded-xl bg-gray-700 p-4">
+                        <div className="flex flex-col gap-5">
+                            <div className="flex items-center">
+                            <div className="m-2 mr-3">
+                                <i className="far fa-redo-alt text-gray-400"></i>
+                            </div>
+                            <div className="flex grow flex-col">
+                                <div className="text-sm font-semibold text-white">Повторить платеж</div>
+                            </div>
+                            <div className="flex flex-col items-end">
+                                <i className="far fa-chevron-right text-gray-400"></i>
+                            </div>
+                            </div>
+                            <div className="flex items-center">
+                            <div className="m-2 mr-3">
+                                <i className="fas fa-receipt text-gray-400"></i>
+                            </div>
+                            <div className="flex grow flex-col">
+                                <div className="text-sm font-semibold text-white">Отправить квитанцию</div>
+                            </div>
+                            <div className="flex flex-col items-end">
+                                <i className="far fa-chevron-right text-gray-400"></i>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                ) : transferResponse.status != 200 ? (
                     <div className="p-4">
                         <div className="mb-5 flex justify-end">
                         <a href="#">
