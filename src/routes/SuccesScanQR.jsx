01af1d671598649375bb0c5cd3557efe4a2816c9
beamingdeamon <51133964+beamingdeamon@users.noreply.github.com>
2022-11-28 02:28:59 +0600

    dsdadsa

--- src/routes/SuccesScanQR.jsx
+++ src/routes/SuccesScanQR.jsx
@@ -1,5 +1,5 @@
 import { useParams, useSearchParams } from "react-router-dom";
-import { useEffect} from 'react';
+import { useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from "axios"
 
